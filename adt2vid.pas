63f68e7bd4bdec5171d86860237b30720e1237d8
Florian Jung <flo@windfisch.org>
2014-04-24 16:40:51 +0200

    cleanups

--- adt2vid.pas
+++ adt2vid.pas
@@ -120,7 +120,9 @@ var
   idx: Byte;
   
 begin
-  EXIT; // this crashes under linux. HACK! TODO!
+{$IFNDEF WINDOWS}
+EXIT; // this crashes under linux. HACK! TODO!
+{$ENDIF}
   If emergency then
     begin        
       For idx := 0 to 15 do
