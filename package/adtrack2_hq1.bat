50bd752fba4e97257e996bee3c9c6a16ce37c29e
Stanislav Baranec <subz3ro.altair@gmail.com>
2014-07-31 10:48:58 +0200

    *** updated SDL 2.4.17 archive ***   - fixed frame rate parameter in batch files

--- package/adtrack2_hq1.bat
+++ package/adtrack2_hq1.bat
@@ -1,2 +1,2 @@
-start adtrack2.exe /cfg:sdl_screen_mode=1 /cfg:sdl_refresh_rate=150
+start adtrack2.exe /cfg:sdl_screen_mode=1 /cfg:sdl_frame_rate=150
 wmic process where name="adtrack2.exe" CALL setpriority "above normal"
--- package/adtrack2_hq2.bat
+++ package/adtrack2_hq2.bat
@@ -1,2 +1,2 @@
-start adtrack2.exe /cfg:sdl_screen_mode=2 /cfg:sdl_refresh_rate=150
+start adtrack2.exe /cfg:sdl_screen_mode=2 /cfg:sdl_frame_rate=150
 wmic process where name="adtrack2.exe" CALL setpriority "high priority"
